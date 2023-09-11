d35dc029001e9bd8ae3a7b60d5afc7b0ea8aab9b
Kaushikkc <kaushik@synlian.net>
2023-09-11 22:10:40 +0530

    Added wallet connection

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,16 @@
 import { useEffect, useMemo, useState } from "react";
+
 import { RPC_ENDPOINT } from "../utils/constants";
+
+
 import { ConnectionProvider, WalletProvider } from "@solana/wallet-adapter-react";
 import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
 import {
   PhantomWalletAdapter,
 
 } from "@solana/wallet-adapter-wallets"
+
+
 import "@solana/wallet-adapter-react-ui/styles.css"
 import "../styles/globals.css";
 function MyApp({ Component, pageProps }) {
@@ -31,11 +36,11 @@ function MyApp({ Component, pageProps }) {
         autoConnect
       >
         <WalletModalProvider>
-        {
-          mounted && (
-            <Component {...pageProps} />
-          )
-        }
+          {
+            mounted && (
+              <Component {...pageProps} />
+            )
+          }
     </WalletModalProvider>
     </WalletProvider>
     </ConnectionProvider>
